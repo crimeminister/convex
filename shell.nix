ae8506cbec797fa0a48db755314ca26ec6bdfde3
Robert Medeiros <robert@crimeminister.org>
2025-01-31 17:14:08 -0500

    chore(build): update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,12 +1,12 @@
 let
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05";
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.11";
   pkgs = import nixpkgs { config = {}; overlays = []; };
 in
 pkgs.mkShellNoCC {
   packages = with pkgs; [
     # Java build tool
     maven
-    # OpenJDK 21
-    jdk21
+    # OpenJDK 23
+    jdk23
   ];
 }
